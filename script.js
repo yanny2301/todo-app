1930b6b87d15ce22ff6f8446cdd9e3138ed6aa79
Yana <yaloginova23@mail.ru>
2026-02-26 14:59:38 +0300

    Add basic HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  if (input.value) {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+  }
+}
